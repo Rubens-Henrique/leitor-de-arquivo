edbd2bec4aa8d0acfb38a5e653148adfc2a63af5
Rubens Henrique <rubenshenrique456@outlook.com>
2023-04-05 16:45:03 -0300

    initial commit

---
+++ src/filme.java
@@ -0,0 +1,54 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class filme {
+  protected     int idFilme;
+  protected  String titulo;
+  protected  float imdb;
+  protected String genero;
+  
+    public filme(int i, String t, float n,String g) {
+          this.idFilme = i;
+          this.titulo = t;
+          this.imdb = n;
+      this.genero = g;
+        }
+
+    public filme() {
+          idFilme = -1;
+          titulo = "";
+          imdb = 0.F;
+          genero="";
+        }
+
+        public String toString() {
+          return "\nID: " + idFilme + 
+                 "\nTítulo: " + titulo + 
+                 "\nimdb:  " + imdb + 
+                "\ngenero:  " +genero ;  
+        }
+
+        public byte[] toByteArray() throws IOException {
+          ByteArrayOutputStream baos = new ByteArrayOutputStream();
+          DataOutputStream dos = new DataOutputStream(baos);
+          dos.writeInt(idFilme);
+          dos.writeUTF(titulo);
+          dos.writeFloat(imdb);
+          dos.writeUTF(genero);
+          return baos.toByteArray();
+        }
+      
+        public void fromByteArray(byte[] ba) throws IOException {
+          ByteArrayInputStream bais = new ByteArrayInputStream(ba);
+          DataInputStream dis = new DataInputStream(bais);
+          idFilme = dis.readInt();
+          titulo = dis.readUTF();
+          imdb = dis.readFloat();
+          genero = dis.readUTF();
+        }
+}
+
+
